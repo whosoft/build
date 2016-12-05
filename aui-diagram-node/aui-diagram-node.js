9fe28225efb094f31ac723fc883543649c6525d8
whosoft <jlh@whosoft.com>
2016-12-06 03:13:57 +0800

    Update aui-diagram-node.js

--- aui-diagram-node/aui-diagram-node.js
+++ aui-diagram-node/aui-diagram-node.js
@@ -141,6 +141,22 @@ DiagramNode = A.Component.create({
         },
 
         /**
+         * The uuid of the node.
+         *
+         * @attribute uuid
+         * @default ''
+         * @type String
+         */
+        uuid: {
+            valueFn: function() {return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+              var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                 return v.toString(16);
+              });
+            },
+            validator: A.Lang.isString
+        },
+
+        /**
          * Stores an instance of `A.Graphic`.
          *
          * @attribute graphic
